99d809185b4885151b06a6b5da1f7f9140434cde
Belal-Wael <belalbatnain050@gmail.com>
2025-04-07 01:53:35 +0200

    make automated test to  formatCurrency function

---
+++ test/monyTest.js
@@ -0,0 +1,29 @@
+// used to test money function  using automated test which mean test by run code in computer.
+import { formatCurrency } from "../Scripts/utils/money.js";
+
+
+// test case 1 (Basic test case)
+if(formatCurrency(2059)==='20.59') 
+    console.log("test pass");
+else 
+console.log("test fail");
+
+
+
+
+
+console.log("work with 0")
+// test case 2 (called Edge case) 
+if(formatCurrency(0)==='0.00')  // not positive or negative 
+    console.log("test pass");
+else 
+console.log("test fail");
+
+
+
+console.log("round up to the nearest cent")
+// test case 2 (called Edge case)
+if(formatCurrency(2000.5)==='20.01')  // without round result will be 20.00 and this miss match
+    console.log("test pass");
+else 
+console.log("test fail");
