5d1015c661e5f20d19971b2b40949e1f160b00ff
Belal-Wael <belalbatnain050@gmail.com>
2025-04-16 20:46:30 +0200

    load products from backend

--- Scripts/checkOut.js
+++ Scripts/checkOut.js
@@ -1,6 +1,10 @@
 import { renderOrderSummary } from "./CheckOut/orderSummary.js";
 import { renderPaymentSummary } from "./CheckOut/paymentSummary.js";
+import { loadProducts } from "../data/products.js";
 //import  '../data/cart-class.js';
-import '../data/backend-test.js'
-renderOrderSummary();
-renderPaymentSummary();
+//import '../data/backend-test.js'
+
+loadProducts(()=>{
+    renderOrderSummary();
+    renderPaymentSummary();
+})
